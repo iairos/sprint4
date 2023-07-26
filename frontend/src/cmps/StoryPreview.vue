6b76a1d0481cfaf3fde09d23081f2f778afcfefb
yifatbe <y7838424@gmail.com>
2023-07-26 19:50:57 +0300

    add preview

--- frontend/src/cmps/StoryPreview.vue
+++ frontend/src/cmps/StoryPreview.vue
@@ -1,9 +1,11 @@
 <template>
     <article class="story-preview">
-        <pre>{{story}}</pre>
+        <!-- <pre>{{story}}</pre> -->
         <img class="user-img" :src="story.by.imgUrl" alt="">
+        <h3>{{ story.by.fullname }} likes</h3>
         <img class="story-img" :src="story.imgUrl" alt="">
-        <span class="svg-icon" v-html="$svg('home')"></span>
+        <!-- <span class="svg-icon" v-html="$svg('home')"></span> -->
+        <h3>{{ story.likedBy.length }} likes</h3>
         <!-- <router-link >comments {{ story.comments.length }}</router-link> -->
         <!-- <button @click="goToDetail">details</button> -->
 
