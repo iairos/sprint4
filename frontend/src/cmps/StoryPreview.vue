d68b16528b12b77574509aa20385b5696cb8b40f
yifatbe <y7838424@gmail.com>
2023-07-26 21:15:13 +0300

    add css

--- frontend/src/cmps/StoryPreview.vue
+++ frontend/src/cmps/StoryPreview.vue
@@ -1,10 +1,16 @@
 <template>
     <article class="story-preview">
         <!-- <pre>{{story}}</pre> -->
-        <img class="user-img" :src="story.by.imgUrl" alt="">
-        <h3>{{ story.by.fullname }} likes</h3>
+        <section class="title">
+            <img class="user-img square-ratio" :src="story.by.imgUrl" alt="">
+            <span class="name fs14">{{ story.by.fullname }}</span>
+        </section>
         <img class="story-img" :src="story.imgUrl" alt="">
-        <span class="svg-icon" v-html="$svg('home')"></span>
+        <span class="svg-icon" v-html="$svg('heart')"></span>
+        <span class="svg-icon" v-html="$svg('comment')"></span>
+        <h3>{{ story.likedBy.length }} likes</h3>
+        <span class="fs14">{{ story.by.fullname }} </span>
+        <span>{{ story.txt }}</span>
         <router-link to="/details/story._id" >comments {{ story.comments.length }}</router-link>
         <!-- <button @click="goToDetail">details</button> -->
 
