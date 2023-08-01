3d927b5bdcb22a8a2dbf4feb4d12f140e6e7cef0
iairos <iairschorr@gmail.com>
2023-08-02 01:48:03 +0300

    small change

--- frontend/src/pages/StoryDetails.vue
+++ frontend/src/pages/StoryDetails.vue
@@ -14,7 +14,7 @@
     <!-- <article class="detail-title flex"> -->
       <article class="user-title header">
         <img class="user-img" :src="story.by.imgUrl" alt="" />
-        <span class="name">{{ story.by.fullname }}</span>
+        <span class="name title">{{ story.by.fullname }}</span>
         <span
         class="svg-icon btn"
         v-html="$svg('threePoints')"
