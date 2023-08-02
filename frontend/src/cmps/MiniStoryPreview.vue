0de6d0a07443fde7468017f46d91338e36d9e95d
yifatbe <y7838424@gmail.com>
2023-08-02 16:31:38 +0300

    user details upgrade

---
+++ frontend/src/cmps/MiniStoryPreview.vue
@@ -0,0 +1,52 @@
+<template>
+  <article class="mini-story-preview"> 
+    <pre>{{story}}</pre>
+    <!-- <img class="img" :src="story.imgsUrl[0]" alt="" /> -->
+    <div class="action">
+      <span    
+        class="svg-icon btn"
+        v-html="$svg('heart')"
+      ></span>
+      <span
+        class="svg-icon btn"
+        v-html="$svg('comment')"
+      ></span>
+      
+    
+</div>
+</article>
+     
+</template>
+
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  props: {
+    story: { type: Object, required: true },
+  },
+  
+  created() {
+    
+  },
+  methods: {
+   
+  },
+  computed: {
+    loggedInUser() {
+      return this.$store.getters.getLoggedInUser;
+    },
+  },
+
+};
+</script>
+
+<style lang="scss">
+
+</style>
